25fb5233d17f64fa497f7f63732550f23c6d07a3
EJ Brown <ejbrown3c@gmail.com>
2021-11-09 20:14:08 -0500

    commit tree f82cfac1436bdc5d3b60f1fac6ea0e9fb14a9921

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+let body = document.querySelector("body")
+console.log(body)
+
+function submitData (name, email) {
+
+   let addingUserData =  {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    body: JSON.stringify({
+        "name":name,
+        "email": email,
+    })
+    }
+
+    return fetch('http://localhost:3000/users', addingUserData) 
+    .then(response => response.json())
+    .then(newUsers => {
+        body.append(newUsers.id)
+    })
+    .catch(function(error){
+        body.append(error.message)
+})
+}
+
